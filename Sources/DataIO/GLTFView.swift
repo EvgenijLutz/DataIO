0b5da08afca481ee8eee5922c350fd77c3b537b5
Evgenij Lutz <evgenij@mac.fritz.box>
2026-06-05 17:51:23 +0200

    Enhance gltf viewer capabilities

--- Sources/DataIO/GLTFView.swift
+++ Sources/DataIO/GLTFView.swift
@@ -8,11 +8,16 @@
 #if canImport(RealityKit)
 
 import SwiftUI
+import ARKit
 import RealityKit
 
 
+typealias Vector3f = SIMD3<Float>
+typealias Quaternionf = simd_quatf
+
+
 @MainActor
-func loadGLTFModel() async throws -> [Entity] {
+func gltfTest() async throws -> [Entity] {
     guard let url = Bundle.main.url(forResource: "SP-50", withExtension: "glb") else {
         print("fuck you")
         return []
@@ -124,45 +129,47 @@ func loadGLTFModel() async throws -> [Entity] {
                 }
             }
             
+            func loadTextureResource(_ imageIndex: Int, _ semantic: TextureResource.Semantic = .color) async throws -> TextureResource? {
+                guard let texture = library.gltf.texture(at: imageIndex) else {
+                    return nil
+                }
+                
+                guard let image = library.gltf.image(at: texture.source) else {
+                    return nil
+                }
+                
+                guard let bufferView = library.gltf.bufferView(at: image.bufferView) else {
+                    return nil
+                }
+                
+                guard let buffer = buffer(at: bufferView.buffer) else {
+                    return nil
+                }
+                
+                let byteOffset = bufferView.byteOffset ?? 0
+                let imageData = Data(buffer.data[byteOffset ..< (byteOffset + bufferView.byteLength)])
+                guard let ciImage = CIImage(data: imageData) else {
+                    return nil
+                }
+                
+                guard let image = ciImage.cgImage else {
+                    return nil
+                }
+                
+                return try await TextureResource(image: image, options: .init(semantic: semantic, mipmapsMode: .allocateAndGenerateAll))
+            }
+            
             // Material
             var materials = [any RealityKit.Material]()
             if let gltfMaterial = library.gltf.material(at: primitive.material) {
                 var material = PhysicallyBasedMaterial()
                 
+                // PBR material
                 if let pbrMetallicRoughness = gltfMaterial.pbrMetallicRoughness {
-                    //material.baseColor = .init(texture: nil)
-                    
+                    // Base colour
                     if let baseColorTexture = pbrMetallicRoughness.baseColorTexture,
-                       let texture = library.gltf.texture(at: baseColorTexture.index),
-                       let image = library.gltf.image(at: texture.source) {
-                        //if let mimeType = image.mimeType {
-                        //    print(mimeType)
-                        //}
-                        //if let uri = image.uri {
-                        //    print(uri)
-                        //}
-                        if let bufferView = library.gltf.bufferView(at: image.bufferView),
-                           let buffer = buffer(at: bufferView.buffer) {
-                            let byteOffset = bufferView.byteOffset ?? 0
-                            let imageData = Data(buffer.data[byteOffset ..< (byteOffset + bufferView.byteLength)])
-                            if let ciImage = CIImage(data: imageData), let image = ciImage.cgImage {
-                                let texture = try await TextureResource(image: image, options: .init(semantic: .color, mipmapsMode: .allocateAndGenerateAll))
-                                material.baseColor = .init(texture: .init(texture))
-                                
-                                if gltfMaterial.alphaMode == .blend {
-                                    // TODO: Use greyscale texture?
-                                    material.blending = .transparent(opacity: .init(scale: 1))
-                                }
-                            }
-                            else {
-                                material.baseColor = .init(tint: .purple)
-                            }
-                        }
-                        else {
-                            // Other variants are not yet supported
-                            // TODO: Implement other texture loading possibilities
-                            material.baseColor = .init(tint: .blue)
-                        }
+                       let texture = try await loadTextureResource(baseColorTexture.index) {
+                        material.baseColor = .init(texture: .init(texture))
                     }
                     else if let baseColor = pbrMetallicRoughness.baseColorFactor, baseColor.count == 4 {
                         let red = CGFloat(baseColor[0])
@@ -175,42 +182,76 @@ func loadGLTFModel() async throws -> [Entity] {
                         
                         material.baseColor = .init(tint: .init(red: red, green: green, blue: blue, alpha: alpha))
                     }
-                    else {
-                        material.baseColor = .init(tint: .yellow)
-                    }
                     
+                    
+                    // Metallic-Roughness
+                    //if let mr = pbrMetallicRoughness.metallicRoughnessTexture,
+                    //   let texture = try await loadTextureResource(mr.index, .raw) {
+                    //    //
+                    //}
+                    
+                    
+                    // Roughness
                     if let roughnessFactor = pbrMetallicRoughness.roughnessFactor {
                         material.roughness = .init(scale: roughnessFactor)
                     }
                     
+                    
+                    // Metallic
                     if let metallicFactor = pbrMetallicRoughness.metallicFactor {
                         material.metallic = .init(scale: metallicFactor)
                     }
-                    
-                    if gltfMaterial.doubleSided == true {
-                        material.faceCulling = .none
-                    }
+                }
+                
+                
+                // Normal map
+                if let normalTexture = gltfMaterial.normalTexture,
+                   let texture = try await loadTextureResource(normalTexture.index, .normal) {
+                    //print("Cunt")
+                    material.normal = .init(texture: .init(texture))
                 }
                 else {
-                    material.baseColor = .init(tint: .red)
+                    material.normal = .init(texture: nil)
                 }
                 
-                material.normal = .init(texture: nil)
+                
+                // Alpha blend mode
+                if gltfMaterial.alphaMode == .blend {
+                    // TODO: Use greyscale texture if set?
+                    material.blending = .transparent(opacity: .init(scale: 1))
+                    material.opacityThreshold = 0.01
+                }
+                
+                
+                // Face culling
+                if gltfMaterial.doubleSided == true {
+                    material.faceCulling = .none
+                }
                 
                 materials.append(material)
-                
-                //materials = [SimpleMaterial(color: .red, isMetallic: true)]
             }
+            materials.append(OcclusionMaterial())
             
             // Vertex attributes
             var numVertices = 0
             var vertexStride = 0
+            
+            var minPosition = Vector3f(-2, -2, -2)
+            var maxPosition = Vector3f(2, 2, 2)
             
             var positionAttribute: LowLevelMesh.Attribute?
             var positionCopyFunction: DataCopyFunction = { _, _ in }
             if let attribute = library.gltf.accessor(at: primitive.attributes["POSITION"]),
                let bufferView = library.gltf.bufferView(at: attribute.bufferView),
                let buffer = buffer(at: bufferView.buffer) {
+                if let min = attribute.min, min.count == 3 {
+                    minPosition = .init(min[0], min[1], min[2])
+                }
+                if let max = attribute.max {
+                    maxPosition = .init(max[0], max[1], max[2])
+                }
+                print("Bounds: \(minPosition) - \(maxPosition)")
+                
                 numVertices = attribute.count
                 let stride = bufferView.byteStride ?? attribute.stride
                 let offset = vertexStride
@@ -286,6 +327,12 @@ func loadGLTFModel() async throws -> [Entity] {
                 }
             }
             
+            
+            if let _ = library.gltf.accessor(at: primitive.attributes["TANGENT"]) {
+                print("Tangent")
+            }
+            
+            
             let vertexAttributes = [positionAttribute, normalAttribute, uvAttribute].compactMap(\.self)
             let vertexLayouts = [LowLevelMesh.Layout(bufferIndex: 0, bufferOffset: 0, bufferStride: vertexStride)]
             
@@ -307,6 +354,7 @@ func loadGLTFModel() async throws -> [Entity] {
                 }
             }
             
+            
             // Fill index data
             if numIndices > 0 {
                 llm.withUnsafeMutableIndices { pointer in
@@ -321,7 +369,7 @@ func loadGLTFModel() async throws -> [Entity] {
             
             // TODO: Calculate bounding box on the fly
             llm.parts.replaceAll([
-                .init(indexCount: numIndices, topology: .triangle, bounds: .init(min: .init(-2, -2, -2), max: .init(2, 2, 2)))
+                .init(indexCount: numIndices, topology: .triangle, bounds: .init(min: minPosition, max: maxPosition))
             ])
             
             let meshResource = try await MeshResource(from: llm)
@@ -343,7 +391,6 @@ func loadGLTFModel() async throws -> [Entity] {
     
     if let scene = library.gltf.scene(at: library.gltf.scene), let nodes = scene.nodes {
         let rootEntity = Entity()
-        rootEntity.transform.translation = .init(0, -0.7, 0)
         entities.append(rootEntity)
         
         func appendNode(_ nodeIndex: Int?, parent: Entity) {
@@ -393,32 +440,102 @@ func loadGLTFModel() async throws -> [Entity] {
 }
 
 
-struct GLTFView: View {
+class ARDelegate: NSObject, ARSessionDelegate {
+    weak var arView: ARView?
+    var anchorsAdded: (_ anchors: [ARAnchor]) -> Void = { _ in }
+    
+    /**
+     This is called when a new frame has been updated.
+    
+     @param session The session being run.
+     @param frame The frame that has been updated.
+     */
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        //print("Did update frame: \(frame)")
+    }
+
+    /**
+     This is called when new anchors are added to the session.
+    
+     @param session The session being run.
+     @param anchors An array of added anchors.
+     */
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        print("Did add anchors: \(anchors)")
+        anchorsAdded(anchors)
+    }
+
+    /**
+     This is called when anchors are updated.
+    
+     @param session The session being run.
+     @param anchors An array of updated anchors.
+     */
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        //print("Did update anchors: \(anchors)")
+    }
+
+    /**
+     This is called when anchors are removed from the session.
+    
+     @param session The session being run.
+     @param anchors An array of removed anchors.
+     */
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        print("Did remove anchors: \(anchors)")
+    }
+}
+
+
+struct GLTFView_Old: View {
+    let arSession = ARSession()
+    let arDelegate = ARDelegate()
+    let worldTracking = ARWorldTrackingConfiguration()
+    
     var body: some View {
         RealityView { content in
             do {
+#if false
                 let env = try await EnvironmentResource(named: "Studio")
                 content.environment = .skybox(env)
+#else
+                content.camera = .spatialTracking
+#endif
+                
+                content.renderingEffects.depthOfField = .enabled
+                content.renderingEffects.motionBlur = .enabled
                 
                 let startTime = CACurrentMediaTime()
-                let entities = try await loadGLTFModel()
+                let entities = try await gltfTest()
                 let elapsed = CACurrentMediaTime() - startTime
                 print("Import time taken: \(elapsed)")
                 
+                if let rootEntity = entities.first {
+                    //rootEntity.transform.translation = .init(0, -0.7, 0)
+                    rootEntity.transform.scale = .init(0.1, 0.1, 0.1)
+                    rootEntity.generateCollisionShapes(recursive: true)
+                }
+                
                 content.entities.append(contentsOf: entities)
                 //print("Num entities: \(entities.count)")
+                
+//                if ARWorldTrackingConfiguration.isSupported {
+//                    worldTracking.planeDetection = [.horizontal, .vertical]
+//
+//                    arSession.run(worldTracking)
+//
+//                    arSession.delegate = arDelegate
+//                }
             }
             catch {
                 print(error)
             }
         }
         .realityViewCameraControls(.orbit)
-        
-//        Text("Cunt")
 //            .task { @concurrent in
 //                do {
 //                    let startTime = CACurrentMediaTime()
-//                    _ = try await loadGLTFModel()
+//                    _ = try await gltfTest()
 //                    let elapsed = CACurrentMediaTime() - startTime
 //                    print("Import time taken: \(elapsed)")
 //                }
@@ -427,6 +544,120 @@ struct GLTFView: View {
 //                }
 //            }
         
+    }
+}
+
+
+struct SysmexView: UIViewRepresentable {
+    typealias UIViewType = ARView
+    var create: (_ view: ARView) -> Void
+    var update: (_ view: ARView) -> Void
+    
+    func makeUIView(context: Context) -> ARView {
+        let view = ARView(frame: .init(x: 0, y: 0, width: 10, height: 10), cameraMode: .ar, automaticallyConfigureSession: false)
+        create(view)
+        return view
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        update(uiView)
+    }
+    
+    
+}
+
+
+struct GLTFView: View {
+    let arSession = ARSession()
+    let arDelegate = ARDelegate()
+    let worldTracking = ARWorldTrackingConfiguration()
+    
+    let anchorEntity = AnchorEntity()
+    var deviceEntity: Entity?
+    
+    
+    func setup(_ anchors: [ARAnchor]) async throws {
+        guard let first = anchors.first as? ARPlaneAnchor else {
+            return
+        }
+        //anchorEntity.transform = Transform(matrix: first.transform)
+        
+        arDelegate.anchorsAdded = { anchors in }
+        
+        let startTime = CACurrentMediaTime()
+        let entities = try await gltfTest()
+        let elapsed = CACurrentMediaTime() - startTime
+        print("Import time taken: \(elapsed)")
+        
+        //let c = AnchorEntity(first)
+        
+        for entity in entities {
+            entity.transform = Transform(matrix: first.transform)
+            
+            //entity.transform.translation = first.center
+            entity.transform.scale = .init(0.3, 0.3, 0.3)
+            entity.generateCollisionShapes(recursive: true)
+            
+            anchorEntity.addChild(entity)
+        }
+    }
+    
+    
+    var body: some View {
+        SysmexView { view in
+            arDelegate.arView = view
+            
+            guard ARWorldTrackingConfiguration.isSupported else {
+                print("World tracking is not supported on this device")
+                return
+            }
+            
+            arDelegate.anchorsAdded = { anchors in
+                Task {
+                    do {
+                        try await setup(anchors)
+                    }
+                    catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+                worldTracking.sceneReconstruction = .mesh
+            }
+            if ARWorldTrackingConfiguration.supportsFrameSemantics(.sceneDepth) {
+                worldTracking.frameSemantics.insert(.sceneDepth)
+            }
+            if ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) {
+                worldTracking.frameSemantics.insert(.personSegmentationWithDepth)
+            }
+            
+            worldTracking.planeDetection = [.horizontal, .vertical]
+            
+            arSession.run(worldTracking)
+            arSession.delegate = arDelegate
+            //view.debugOptions = [.showAnchorOrigins]
+            view.session = arSession
+            
+            view.environment.sceneUnderstanding.options = [.collision, .occlusion]
+            view.scene.addAnchor(anchorEntity)
+            
+        } update: { view in
+            //
+        }
+        .gesture(DragGesture().onChanged { value in
+            guard let view = arDelegate.arView else {
+                return
+            }
+            
+            let results = view.raycast(from: value.location, allowing: .existingPlaneGeometry, alignment: .horizontal)
+            print(results)
+            
+            if let first = results.first, let rootEntity = anchorEntity.children.first {
+                rootEntity.transform.translation = Transform(matrix: first.worldTransform).translation
+            }
+        })
     }
 }
 
